b7f62687a30246c8125ef87ba9ab4519e6c24ac1
seanyboyswag <2150218@slu.edu.ph>
2020-03-31 17:44:34 +0800

    Redesigned UserForm

--- resources/js/components/admin/UserForm.js
+++ resources/js/components/admin/UserForm.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {Button, Col, Row, Alert, Form} from "react-bootstrap";
 
 const UserForm = (props) => {
 
@@ -24,7 +25,7 @@ const UserForm = (props) => {
     }
 
     useEffect(() => {
-        if(props.userData) {
+        if (props.userData) {
             setUserName(props.userData.name);
             setUserRole(props.userData.role);
             setUserEmail(props.userData.email);
@@ -33,33 +34,89 @@ const UserForm = (props) => {
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                {error && <p className="form__error">{error}</p>}
-                <input
-                    className="text-input"
-                    type="text"
-                    placeholder="Name"
-                    value={userName || ''}
-                    onChange={(e) => setUserName(e.target.value)}
-                />
-                <input
-                    className="text-input"
-                    type="text"
-                    placeholder="Role"
-                    value={userRole || ''}
-                    onChange={(e) => setUserRole(e.target.value)}
-                />
-                <input
-                    className="text-input"
-                    type="email"
-                    placeholder="Email"
-                    value={userEmail || ''}
-                    onChange={(e) => setUserEmail(e.target.value)}
-                />
-                <button className="button">Save user</button>
-            </form>
+
+            <Form onSubmit={onSubmit}>
+                {error && (
+                    <Alert key={error} variant='danger'>
+                        {error}
+                    </Alert>
+                )}
+
+                <Form.Group controlId="formName">
+                    <Form.Label>
+                        User name
+                    </Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter name"
+                        value={userName || ''}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formEmail">
+                    <Form.Label>
+                        Email address
+                    </Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={userEmail || ''}
+                        onChange={(e) => setUserEmail(e.target.value)}
+                    />
+                    <Form.Text className="text-muted">
+                        Email must be unique for each user.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formEmail">
+                    <Form.Label>
+                        Role
+                    </Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter role"
+                        value={userRole || ''}
+                        onChange={(e) => setUserRole(e.target.value)}
+                    />
+                    <Form.Text className="text-muted">
+                        User role in the system
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Save User
+                </Button>
+            </Form>
+
+
+            {/*<form onSubmit={onSubmit}>*/}
+
+                {/*<input*/}
+                    {/*className="text-input"*/}
+                    {/*type="text"*/}
+                    {/*placeholder="Name"*/}
+                    {/*value={userName || ''}*/}
+                    {/*onChange={(e) => setUserName(e.target.value)}*/}
+                {/*/>*/}
+                {/*<input*/}
+                    {/*className="text-input"*/}
+                    {/*type="text"*/}
+                    {/*placeholder="Role"*/}
+                    {/*value={userRole || ''}*/}
+                    {/*onChange={(e) => setUserRole(e.target.value)}*/}
+                {/*/>*/}
+                {/*<input*/}
+                    {/*className="text-input"*/}
+                    {/*type="email"*/}
+                    {/*placeholder="Email"*/}
+                    {/*value={userEmail || ''}*/}
+                    {/*onChange={(e) => setUserEmail(e.target.value)}*/}
+                {/*/>*/}
+                {/*<button className="button">Save user</button>*/}
+            {/*</form>*/}
         </div>
     )
 }
 
-export { UserForm as default }
+export {UserForm as default}
